f5ec9eb0346853e7ed7cd5f722cc6d99f68c661f
Marco <mrondelli94@gmail.com>
2018-11-23 18:35:18 +0100

    stargazer header

--- app/src/main/java/com/marco/stargazers/models/Repo.kt
+++ app/src/main/java/com/marco/stargazers/models/Repo.kt
@@ -6,4 +6,5 @@ data class Repo(
     @SerializedName("name") val name: String,
     @SerializedName("description") val description: String?,
     @SerializedName("language") val language: String?,
-    @SerializedName("stargazers_count") val stargazers : Number)
+    @SerializedName("stargazers_count") val stargazers : Number,
+    @SerializedName("avatar_url") val ownerImageUrl : String)
